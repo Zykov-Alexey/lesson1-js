7b16680069a7bb2f328ffb31f9c31cf7c7dbb444
Alexey Zykov <zykov.alexey@gmail.com>
2020-03-14 20:21:03 +0200

    1st dz JS

---
+++ js/main.js
@@ -0,0 +1,48 @@
+console.log('Sample JavaScript HW#1');
+
+var myNum = 10;
+var myStr = 'строка';
+var myBool =  true;
+var myArr = [1, 2, 3, 4, 5];
+
+var myObj = {first:'First Name', last:'Last Name'};
+
+var decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+
+var i = 200;
+console.log(++i);
+console.log(i++);
+
+console.log(--i);
+console.log(i--);
+
+var myTest = 20;
+    myTest +=20;
+  console.log(myTest);
+    myTest -=5;
+  console.log(myTest);
+    myTest *=myNum;
+  console.log(myTest);
+    myTest /=5;
+  console.log(myTest);
+    myTest %=4;
+  console.log(myTest);
+
+
+const Pi = Math.PI;
+var myPi = Pi;
+    console.log(myPi);
+    
+const MyRound = Math.round(89.279);
+var myRound = MyRound;
+  console.log(myRound);
+
+const MyRandom = Math.random()*10;
+var myRandom = MyRandom;
+  console.log(myRandom); 
+
+
+  
+  // console.log(Math.random()*10);
+  // console.log(Math.;
